495b37b12107b82897505e56460502bfe515a398
Igor Batista <igorbatista.dev@gmail.com>
2025-06-21 03:04:45 -0300

    feat: Adiciona listagem de produtos

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const config = require('./config/config.json')['development'];
 const app = express();
 const port = 3000;
 
-// Sequelize
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 const Produto = require('./models/produto')(sequelize, DataTypes);
 
@@ -21,7 +20,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/produtos/novo', (req, res) => {
-  res.render('produtos/form', { product: {}, action: '/produtos' });
+  res.render('produtos/form', { produto: {}, action: '/produtos' });
 });
 
 app.post('/produtos', async (req, res) => {
@@ -38,13 +37,19 @@ app.post('/produtos', async (req, res) => {
       preco: preco
     });
 
-    res.redirect('/');
+    res.redirect('/produtos');
 
   } catch (error) {
     console.error('Erro ao criar produto:', error);
     res.status(500).send('Erro ao criar produto');
   }
 });
+
+app.get('/produtos', async (req, res) => {
+  const produtos = await Produto.findAll();
+  res.render('produtos/lista', { produtos });
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
